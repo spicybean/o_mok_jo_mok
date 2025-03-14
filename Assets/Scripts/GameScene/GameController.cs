eaf8198d1355531ac6833cd60c7c9e8c52e9fb5e
spicybean <spicybean0108@gmail.com>
2025-03-14 11:44:41 +0900

    테스트 스크립트 변경 오목판에 오목 플레이싱 글자에서 이미지로 변경

--- Assets/Scripts/GameScene/GameController.cs
+++ Assets/Scripts/GameScene/GameController.cs
@@ -5,102 +5,113 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public struct Cell
-{
-    public int index;
-    public int turn;
-    public string name;
-    public GameObject cellObject;
-}
+
 
 public class GameController : MonoBehaviour, IPointerClickHandler
 {
+    public enum playerType{None,PlaceMark,Black, White}
     public GameObject[] omokButtons;
-    public Cell[,] omokBoard;
-    private Color myTurn = Color.black;
-    private Color enemyTurn = Color.blue;
-    private string myturnName = "Black";
-    private string enemyturnName = "White";
+    public playerType[,] omokBoard;
+    
     public GameObject omokPrefab;
     private int totalomokCells;
-    private int col;
     private int turncounter;
-    public enum playerType{Black, White}
+    public GameObject selectedCell;
     public playerType turn;
+   
     // Start is called before the first frame update
     void Start()
     {
+        selectedCell = null;
         turncounter = 0;
-        totalomokCells = gameObject.transform.childCount;
+        totalomokCells = 15*15;
         omokButtons = new GameObject[totalomokCells];
-        omokBoard = new Cell[totalomokCells/7, 7];
+        omokBoard = new playerType[15, 15];
         Debug.Log($"{totalomokCells}, {7}");
         //foreach (Transform tr in transform)Debug.Log(tr.name);
         for (int i = 0; i < totalomokCells; i++)
         {
-            omokButtons[i] = gameObject.transform.GetChild(i).GetComponent<Image>().gameObject;
-            omokButtons[i].transform.GetChild(0).GetComponent<TMP_Text>().text = "";
-            omokBoard[i/7, i%7].index = i;
-            omokBoard[i / 7, i % 7].name = "";
-            omokBoard[i/7, i%7].cellObject = transform.GetChild(i).gameObject;
-            Debug.Log($"{i/7}, {i%7},{transform.GetChild(i).gameObject.name}");
-            
+            var OmokObejct = Instantiate(omokPrefab, transform);
+            omokButtons[i] = OmokObejct;
+            omokButtons[i].GetComponent<OmokCell>().initCell(i);
+            omokBoard[i / 15, i % 15] = playerType.None;
+
         }
         
         
         turn = playerType.Black;
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void StartGame()
     {
         
     }
-
-    void SetMarker(int index, string name)
+    void CheckOmok(int row, int col)
     {
+        (int,int) center = (row,col);
+        (int,int)[] directions = new (int,int)[]{(0,1),(1,0),(1,1),(-1,1)};
         
+    }
+    
+    void CheckDoubleThreeFour(int row, int col)
+    {
+        (int,int)[] directions = new (int,int)[]{(0,1),(1,0),(1,1),(-1,1)};
+        
+        //row >= 0 && row <= Fullindexsize/ 7
+        //row count 4 from center to every direction
+        //col >= 0 && col <= Fullindexsize % 7
+        //col count 4 from center to every direction
+        //만약 가로,세로,대각선중 중심에 돌을 두었을시 오목이 되면 적용 안됨
+        //중심을 기준으로 3칸씩 총 6칸 사이에 흰돌이 없으면 4.4 3.3 적용 아니면 괜찮
+        //단, 그 중심점에 두었을때 오목이 된다면 무시 가능
+        //단, 각 
+        //3.3일때
+        
+        //3.4일때
+        //4.4일때
     }
     void SetTurn(playerType player, int index)
     {
         switch (player)
         {
             case playerType.Black:
-                if (omokBoard[index / 7, index % 7].name != "") return;
-                omokBoard[index/7, index%7].cellObject.GetComponentInChildren<TMP_Text>().text = myturnName;
-                omokBoard[index / 7, index % 7].name = myturnName;
-                Debug.Log(omokButtons[index].gameObject.GetComponentInChildren<TMP_Text>().text);
-                turn = turn == playerType.Black ? playerType.White : playerType.Black;
+                if (omokBoard[index / 15, index % 15] != playerType.None) return;
+                omokBoard[index / 15, index % 15] = player;
                 turncounter++;
-                Debug.Log($"{turncounter}");
+                omokButtons[index].GetComponent<OmokCell>().PlaceMark(OmokCell.MarkerType.Black,turncounter);
+                turn = turn == playerType.Black ? playerType.White : playerType.Black;
                 break;
             case playerType.White:
-                if (omokBoard[index / 7, index % 7].name != "") return;
-                omokBoard[index/7, index%7].cellObject.GetComponentInChildren<TMP_Text>().text = enemyturnName;
-                omokBoard[index / 7, index % 7].name = enemyturnName;
-                Debug.Log(omokButtons[index].gameObject.GetComponentInChildren<TMP_Text>().text);
-                turn = turn == playerType.Black ? playerType.White : playerType.Black;
+                if (omokBoard[index / 15, index % 15] != playerType.None) return;
+                omokBoard[index / 15, index % 15] = player;
                 turncounter++;
+                omokButtons[index].GetComponent<OmokCell>().PlaceMark(OmokCell.MarkerType.White,turncounter);
+                turn = turn == playerType.Black ? playerType.White : playerType.Black;
                 break;
         }
     }
-    
+
+   
     public void OnPointerClick(PointerEventData eventData)
     {
-        Debug.Log("OnPointerClick" + name);
+        
+        //Debug.Log("OnPointerClick" + name);
         if (PointerEventData.InputButton.Left == eventData.button)
         {
-            var name = eventData.pointerCurrentRaycast.gameObject.transform.parent.name;
-            for (int i = 0; i < omokButtons.Length; i++)
+            var Cell = eventData.pointerCurrentRaycast.gameObject.GetComponent<OmokCell>();
+            var previousCellSelected = selectedCell == null ? 
+                Cell.gameObject : selectedCell;
+            selectedCell = Cell.gameObject;
+            if (previousCellSelected.GetComponent<Image>().sprite == selectedCell.GetComponent<Image>().sprite)
             {
-                if (name == omokButtons[i].name)
-                {
-                    
-                    SetTurn(turn, i);
-                    
-                }
+                SetTurn(turn,Cell.index);
             }
+            else
+            {
+                selectedCell.GetComponent<OmokCell>().PlaceMark(OmokCell.MarkerType.PlaceMark,turncounter);
+            }
+            
+            
         }
     }
 }
---
+++ Assets/Scripts/GameScene/OmokCell.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OmokCell : MonoBehaviour
+{
+    public enum MarkerType{None,PlaceMark,Black,White}
+    [SerializeField] private Sprite _blackStone;
+    [SerializeField] private Sprite _whiteStone;
+    [SerializeField] private Sprite _selectedSprite;
+    [SerializeField] private MarkerType _markerType;
+    [SerializeField] private Color _selectedColor;
+    
+    public int index;
+    public int placedTurn;
+
+    public int INDEX
+    {
+        get { return index; }
+        set { }
+    }
+
+    public MarkerType GetMarkerType{
+        get
+        {
+            return _markerType;
+        }
+        
+    }
+    private delegate void OnStoneCellClicked(int index);
+    private OnStoneCellClicked _onStoneCellClicked;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public void initCell(int index)
+    {
+        this.index = index;
+        this.placedTurn = 0;
+        _markerType = MarkerType.None;
+        _selectedColor = new Color(1, 1, 1, 1);
+    }
+    public void SetTurn(int turn)
+    {
+        placedTurn = turn;
+    }
+    
+    public void PlaceMark(MarkerType playeMarkerTypeType, int turn)
+    {
+        switch (playeMarkerTypeType)
+        {
+            case MarkerType.Black:
+                this.GetComponent<Image>().sprite = _blackStone;
+                GetComponent<Image>().color = _selectedColor;
+                _markerType = MarkerType.Black;
+                placedTurn = turn;
+                //GetComponent<SpriteRenderer>().sprite = _blackStone;
+                break;
+            case MarkerType.White:
+                GetComponent<Image>().sprite = _whiteStone;
+                GetComponent<Image>().color = _selectedColor;
+                _markerType = MarkerType.White;
+                placedTurn = turn;
+                break;
+            case MarkerType.PlaceMark:
+                this.GetComponent<Image>().sprite = _selectedSprite;
+                GetComponent<Image>().color = _selectedColor;
+                _markerType = MarkerType.PlaceMark;
+                break;
+            case MarkerType.None:
+                this.GetComponent<Image>().sprite = null;
+                GetComponent<Image>().color = new Color(0, 0, 0, 0f);
+                _markerType = MarkerType.None;
+                break;
+                
+        }
+    }
+    public void OnClickedStoneCell()
+    {
+        //_onStoneCellClicked?.Invoke(index);
+        //_stoneIndexText.text = index.ToString();
+        
+
+    }
+    
+}
